f3aae97b1dd9225e07a9d79e46a1785293054769
amarchino <marchino.alessandro@gmail.com>
2025-12-27 22:40:07 +0100

    Lesson 15

---
+++ README.md
@@ -0,0 +1,3 @@
+# Spring AI: Beginner to Guru
+
+Companion repository for the Udemy course [Spring AI: Beginner to Guru](https://www.udemy.com/course/spring-ai-beginner-to-guru/)
---
+++ springaiintro/src/main/java/guru/springframework/springaiintro/SpringaiintroApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.springaiintro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringaiintroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringaiintroApplication.class, args);
+	}
+
+}
