3456cd2fb8d45b0dea0fbc7a1c9c986eeea5da07
amarchino <marchino.alessandro@gmail.com>
2025-12-29 13:44:24 +0100

    Lesson 20

--- springaiintro/src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
+++ springaiintro/src/main/java/guru/springframework/springaiintro/controllers/QuestionController.java
@@ -6,17 +6,22 @@ import guru.springframework.springaiintro.model.Answer;
 import guru.springframework.springaiintro.model.Question;
 import guru.springframework.springaiintro.services.OllamaService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class QuestionController {
 
     private final OllamaService ollamaService;
 
-    @PostMapping
-    public Answer askQuestion(Question question) {
+    @PostMapping("/ask")
+    public Answer askQuestion(@RequestBody Question question) {
+        log.info("Invoked askQuestion");
         return ollamaService.getAnswer(question);
     }
 
