752946cfd0e79462e4be33a17ac2b55299c6232e
amarchino <marchino.alessandro@gmail.com>
2025-12-30 23:10:00 +0100

    Lesson 40

--- ai-rag/src/main/java/guru/springframework/airag/config/VectorStoreConfig.java
+++ ai-rag/src/main/java/guru/springframework/airag/config/VectorStoreConfig.java
@@ -1,18 +1,43 @@
 package guru.springframework.airag.config;
 
+import java.io.File;
+import java.util.List;
+
+import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.ai.reader.tika.TikaDocumentReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.lang.NonNull;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class VectorStoreConfig {
 
+    @SuppressWarnings("null")
     @Bean
     SimpleVectorStore simpleVectorStore(@NonNull EmbeddingModel embeddingModel, VectorStoreProperties vectorStoreProperties) {
-        return SimpleVectorStore.builder(embeddingModel)
+        SimpleVectorStore store = SimpleVectorStore.builder(embeddingModel).build();
+        File vectorStoreFile = new File(vectorStoreProperties.getVectorStorePath());
+        if(vectorStoreFile.exists()) {
+            store.load(vectorStoreFile);
+        } else {
+            log.info("Loading documents into vector store");
+            vectorStoreProperties.getDocumentsToLoad().forEach(document -> {
+                log.info("Loading document: {}", document.getFilename());
+                TikaDocumentReader documentReader = new TikaDocumentReader(document);
+                List<Document> docs = documentReader.get();
+                TextSplitter textSplitter = new TokenTextSplitter();
+                List<Document> splitDocs = textSplitter.apply(docs);
+                store.add(splitDocs);
+            });
+        }
 
-            .build();
+        return store;
     }
 }
--- ai-rag/src/main/java/guru/springframework/airag/config/VectorStoreProperties.java
+++ ai-rag/src/main/java/guru/springframework/airag/config/VectorStoreProperties.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 
 import lombok.Data;
 
@@ -13,5 +14,5 @@ import lombok.Data;
 public class VectorStoreProperties {
 
     private String vectorStorePath;
-    private List<String> documentsToLoad;
+    private List<Resource> documentsToLoad;
 }
