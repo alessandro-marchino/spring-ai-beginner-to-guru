b73614e82b6f7898c08a3b2e09aa8668ae216644
amarchino <marchino.alessandro@gmail.com>
2025-12-30 19:50:07 +0100

    Lesson 32

--- promptengineering/src/main/java/guru/springframework/promptengineering/service/BaseChatService.java
+++ promptengineering/src/main/java/guru/springframework/promptengineering/service/BaseChatService.java
@@ -2,8 +2,13 @@ package guru.springframework.promptengineering.service;
 
 import java.util.Map;
 
+import org.springframework.ai.chat.prompt.ChatOptions;
+import org.springframework.lang.Nullable;
+
 public interface BaseChatService {
 
     String chat(String prompt);
+    String chat(String prompt, @Nullable ChatOptions chatOptions);
     String chat(String prompt, Map<String, Object> params);
+    String chat(String prompt, Map<String, Object> params, @Nullable ChatOptions chatOptions);
 }
--- promptengineering/src/main/java/guru/springframework/promptengineering/service/BaseChatServiceImpl.java
+++ promptengineering/src/main/java/guru/springframework/promptengineering/service/BaseChatServiceImpl.java
@@ -3,8 +3,10 @@ package guru.springframework.promptengineering.service;
 import java.util.Map;
 
 import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Service;
 
 import lombok.RequiredArgsConstructor;
@@ -16,16 +18,23 @@ public class BaseChatServiceImpl implements BaseChatService {
 
     @Override
     public String chat(String prompt) {
-        PromptTemplate promptTemplate = new PromptTemplate(prompt);
-        Prompt promptToSend = promptTemplate.create();
+        return chat(prompt, Map.of(), null);
+    }
 
-        return chatModel.call(promptToSend).getResult().getOutput().getText();
+    @Override
+    public String chat(String prompt, @Nullable ChatOptions chatOptions) {
+        return chat(prompt, Map.of(), chatOptions);
     }
 
     @Override
     public String chat(String prompt, Map<String, Object> params) {
+        return chat(prompt, params, null);
+    }
+
+    @Override
+    public String chat(String prompt, Map<String, Object> params, @Nullable ChatOptions chatOptions) {
         PromptTemplate promptTemplate = new PromptTemplate(prompt);
-        Prompt promptToSend = promptTemplate.create(params);
+        Prompt promptToSend = promptTemplate.create(params, chatOptions);
         return chatModel.call(promptToSend).getResult().getOutput().getText();
     }
 
